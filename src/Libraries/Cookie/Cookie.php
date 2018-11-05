5afeeb2e5d501c75594cbce1f8eae245d1271054
armanist <armanist@mail.ru>
2018-11-05 17:43:10 +0400

    Cookie Library

---
+++ src/Libraries/Cookie/Cookie.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ * 
+ * An open source software development framework for PHP
+ * 
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 1.0.0
+ */
+
+namespace Quantum\Libraries\Cookie;
+
+use Quantum\Exceptions\ExceptionMessages;
+use Quantum\Routes\RouteController;
+
+/**
+ * Cookie Class
+ * 
+ * @package Quantum
+ * @subpackage Libraries.Cookie
+ * @category Libraries
+ */
+class Cookie {
+
+    /**
+     * Gets value from cookie by key
+     * 
+     * @param string $key
+     * @return mixed
+     */
+    public static function get($key) {
+        return self::has($key) ? self::decode($_COOKIE[$key]) : NULL;
+    }
+
+    /*
+     * Gets whole cookie data
+     */
+
+    public static function all() {
+        $allCookies = array();
+        if (isset($_COOKIE) && count($_COOKIE)) {
+            foreach ($_COOKIE as $key => $value) {
+                $allCookies[$key] = self::decode($value);
+            }
+        }
+        return $allCookies;
+    }
+
+    /**
+     * Sets cookie data by key
+     * 
+     * @param string $key
+     * @param string $value
+     * @param integer $time
+     * @param string $path
+     * @param string $domain
+     * @param boolean $secure
+     * @param boolean $httponly
+     * @return bool
+     */
+    public static function set($key, $value = '', $time = 0, $path = '/', $domain = '', $secure = FALSE, $httponly = FALSE) {
+        return setcookie($key, self::encode($value), time() + $time, $path, $domain, $secure, $httponly);
+    }
+
+    /**
+     * Check if cookie contains a data by key
+     * 
+     * @param string $key
+     * @return bool
+     */
+    public static function has($key) {
+        return isset($_COOKIE[$key]) ? true : false;
+    }
+
+    /**
+     * Delete cookie data by given key
+     * 
+     * @param string $key
+     * @param string $path
+     */
+    public static function delete($key, $path = '/') {
+        if (self::has($key)) {
+            setcookie($key, '', time() - 3600, $path);
+        }
+    }
+
+    /**
+     * Deletes whole cookie data
+     */
+    public static function flush() {
+        if (count($_COOKIE) > 0) {
+            foreach ($_COOKIE as $key => $value) {
+                self::delete($key, '/');
+            }
+        }
+    }
+
+    /**
+     * Encodes the cookie data
+     * 
+     * @param string $value
+     * @return string
+     */
+    private static function encode($value) {
+        return base64_encode($value);
+    }
+
+    /**
+     * Decodes the cookie data
+     * 
+     * @param string $value
+     * @return string
+     */
+    public static function decode($value) {
+        return base64_decode($value);
+    }
+
+}
