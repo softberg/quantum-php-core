081528b955add10926e9f50c9c9b6aa6d066fa47
Arman <armanist@mail.ru>
2019-11-14 18:18:50 +0400

    Hasher class and unit test

---
+++ src/Libraries/Hasher/Hasher.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 1.8.0
+ */
+
+namespace Quantum\Libraries\Hasher;
+
+/**
+ * Hasher class
+ *
+ * @package Quantum
+ * @subpackage Libraries.Hasher
+ * @category Libraries
+ */
+class Hasher
+{
+
+    /**
+     * The algorithm
+     * 
+     * @var int 
+     */
+    private $algorithm = PASSWORD_BCRYPT;
+
+    /**
+     * The cost
+     * 
+     * @var int 
+     */
+    private $cost = 12;
+
+    /**
+     * Sets the algorithm
+     * 
+     * @param int $algorithm
+     * @return $this
+     */
+    public function setAlgorithm($algorithm)
+    {
+        $this->algorithm = $algorithm;
+        return $this;
+    }
+
+    /**
+     * Gets the current algorithm 
+     * 
+     * @return int
+     */
+    public function getAlgorithm()
+    {
+        return $this->algorithm;
+    }
+
+    /**
+     * Sets the cost
+     * 
+     * @param int $cost
+     * @return $this
+     */
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+        return $this;
+    }
+
+    /**
+     * Gets the current cost
+     * 
+     * @return int
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * Hashes the given string
+     * 
+     * @param string $password
+     * @return string
+     */
+    public function hash($password)
+    {
+        return password_hash($password, $this->algorithm, ['cost' => $this->cost]);
+    }
+
+    /**
+     * Checks if re-hash needed
+     * 
+     * @param string $hash
+     * @return bool
+     */
+    public function needsRehash($hash)
+    {
+        return password_needs_rehash($hash, $this->algorithm);
+    }
+
+    /**
+     * Checks the given string against the hash
+     * 
+     * @param string $password
+     * @param string $hash
+     * @return bool
+     */
+    public function check($password, $hash)
+    {
+        return password_verify($password, $hash);
+    }
+
+    /**
+     * Gets an info of given hash
+     * 
+     * @param string $hash
+     * @return array()
+     */
+    public function info($hash)
+    {
+        return password_get_info($hash);
+    }
+
+}
---
+++ tests/unit/Libraries/Hasher/HasherTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Quantum\Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Quantum\Libraries\Hasher\Hasher;
+
+class HasherTest extends TestCase
+{
+
+    private $hasher;
+    private $password = 'plaintext';
+    private $otherPassword = 'other';
+
+    public function setUp(): void
+    {
+        $this->hasher = new Hasher();
+    }
+
+    public function testSetAndGetAlgorithm()
+    {
+        $this->assertEquals(PASSWORD_BCRYPT, $this->hasher->getAlgorithm());
+
+        $this->hasher->setAlgorithm(PASSWORD_ARGON2I);
+
+        $this->assertEquals(PASSWORD_ARGON2I, $this->hasher->getAlgorithm());
+    }
+
+    public function testSetAndGetCost()
+    {
+        $this->assertEquals(12, $this->hasher->getCost());
+
+        $this->hasher->setCost(14);
+
+        $this->assertEquals(14, $this->hasher->getCost());
+    }
+
+    public function testHashAndCheck()
+    {
+        $hashed = $this->hasher->hash($this->password);
+
+        $this->assertTrue($this->hasher->check($this->password, $hashed));
+
+        $this->assertFalse($this->hasher->check($this->otherPassword, $hashed));
+    }
+
+    public function testNeedsRehash()
+    {
+        $hashed = $this->hasher->hash($this->password);
+
+        $this->assertTrue($this->hasher->needsRehash($hashed));
+
+        $this->hasher->setAlgorithm(PASSWORD_ARGON2I);
+
+        $this->hasher->setCost(11);
+
+        $this->assertTrue($this->hasher->needsRehash($hashed));
+    }
+
+    public function testInfo()
+    {
+        $hashed = $this->hasher->hash($this->password);
+
+        $this->assertIsArray($this->hasher->info($hashed));
+
+        $this->assertArrayHasKey('algoName', $this->hasher->info($hashed));
+    }
+
+}
