945b06a5011cbbedcaab45a975a4c9da458db5a4
Arman <arman.aghabekyan@gmail.com>
2020-01-18 19:21:13 +0400

    Adding new ModelException

---
+++ src/Exceptions/ModelException.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ * 
+ * An open source software development framework for PHP
+ * 
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 1.9.5
+ */
+
+namespace Quantum\Exceptions;
+
+/**
+ * ServiceException class
+ * 
+ * @package Quantum
+ * @subpackage Exceptions
+ * @category Exceptions
+ */
+class ModelException extends \Exception
+{
+    
+}
