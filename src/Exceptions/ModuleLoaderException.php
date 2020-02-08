d69be0bc46087f3b1bc8988e44f5303ce46f86b5
Arman <armanist@mail.ru>
2020-02-08 17:21:10 +0400

    Adding new ModuleLoaderException exception

---
+++ src/Exceptions/ModuleLoaderException.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ * 
+ * An open source software development framework for PHP
+ * 
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 1.9.5
+ */
+
+namespace Quantum\Exceptions;
+
+/**
+ * ModuleLoaderException class
+ * 
+ * @package Quantum
+ * @category Exceptions
+ */
+class ModuleLoaderException extends \Exception
+{
+    
+}
