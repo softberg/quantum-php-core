6ae41f2201d9d0c709c9edefc9574d87ef3a6a27
Arman <armanist@mail.ru>
2020-07-12 21:18:50 +0400

    Unit testing - Lang library

--- tests/unit/Libraries/Lang/LangTest.php
+++ tests/unit/Libraries/Lang/LangTest.php
@@ -1,84 +1,143 @@
 <?php
 
-namespace Quantum\Test\Unit;
+namespace Quantum\Libraries\Lang {
 
-use Mockery;
-use PHPUnit\Framework\TestCase;
-use Quantum\Libraries\Lang\Lang;
-
-
-class LangTest extends TestCase
-{
-
-    private $loader;
-
-    private $langData = [
-        'learn_more' => 'Learn more',
-        'info' => 'Information about {%1} feature',
-        'test' => 'Testing'
-    ];
-
-
-    public function setUp(): void
+    function _message($subject, $params)
     {
-        $this->loader = Mockery::mock('Quantum\Loader\Loader');
+        if (is_array($params)) {
+            return preg_replace_callback('/{%\d+}/', function () use (&$params) {
+                return array_shift($params);
+            }, $subject);
+        } else {
+            return preg_replace('/{%\d+}/', $params, $subject);
+        }
     }
 
-    public function tearDown(): void
+    function modules_dir()
     {
-        Mockery::close();
+        return __DIR__ . DS . 'modules';
     }
 
-    public function testLangLoad()
+    function current_module()
     {
-        $this->assertEmpty(Lang::getTranslations());
-
-        $this->loader->shouldReceive('load')
-            ->andReturn($this->langData);
-
-        Lang::load($this->loader, 'common');
-
-        $this->assertNotEmpty(Lang::getTranslations());
-
-    }
-
-    public function testLangGetSet()
-    {
-        $this->assertNull(Lang::get());
-
-        Lang::set('en');
-
-        $this->assertEquals('en', Lang::get());
-    }
-
-    public function testGetTranslation()
-    {
-        $this->assertEquals('Testing', Lang::getTranslation('common.test'));
-
-        $this->assertEquals('Information about new feature', Lang::getTranslation('common.info', 'new'));
-
-        $this->assertEquals('common.not-exists', Lang::getTranslation('common.not-exists'));
-
-        $this->loader->shouldReceive('load')
-            ->andReturn($this->langData);
-
-        Lang::load($this->loader, 'other');
-
-        $this->assertEquals('Testing', Lang::getTranslation('other.test'));
-
+        return 'test';
     }
 
 }
 
-namespace Quantum\Libraries\Lang;
+namespace Quantum\Test\Unit {
 
-function _message($subject, $params)
-{
-    if (is_array($params)) {
-        return preg_replace_callback('/{%\d+}/', function () use (&$params) {
-            return array_shift($params);
-        }, $subject);
-    } else {
-        return preg_replace('/{%\d+}/', $params, $subject);
+    use Mockery;
+    use PHPUnit\Framework\TestCase;
+    use Quantum\Libraries\Lang\Lang;
+    use Quantum\Loader\Loader;
+
+    class LangTest extends TestCase
+    {
+
+        private $lang;
+        private $langDir;
+        private $loaderMock;
+        private $langData = [
+            'learn_more' => 'Learn more',
+            'info' => 'Information about {%1} feature',
+            'test' => 'Testing'
+        ];
+
+        public function setUp(): void
+        {
+            $this->loaderMock = Mockery::mock('Quantum\Loader\Loader');
+
+            $this->loaderMock->shouldReceive('setup')->andReturn($this->loaderMock);
+
+            $this->loaderMock->shouldReceive('load')->andReturn($this->langData);
+
+            $loader = new Loader();
+
+            $loader->loadDir(dirname(__DIR__, 4) . DS . 'src' . DS . 'Helpers' . DS . 'functions');
+
+            $loader->loadFile(dirname(__DIR__, 4) . DS . 'src' . DS . 'constants.php');
+
+            config()->set('langs', ['en', 'ru', 'am']);
+
+            config()->set('lang_default', 'en');
+
+            config()->set('lang_segment', 1);
+
+            $this->langDir = \Quantum\Libraries\Lang\modules_dir() . DS . \Quantum\Libraries\Lang\current_module() . DS . 'Resources' . DS . 'lang' . DS . 'en';
+
+            if (!is_dir($this->langDir)) {
+                mkdir($this->langDir, 0777, true);
+            }
+
+            file_put_contents($this->langDir . DS . 'custom.php', null);
+
+            $this->lang = Lang::getInstance();
+
+            $this->lang->setLang('en');
+
+            $reflectionClass = new \ReflectionClass(Lang::class);
+
+            $reflectionProperty = $reflectionClass->getProperty('translations');
+
+            $reflectionProperty->setAccessible(true);
+
+            $reflectionProperty->setValue([]);
+        }
+
+        public function tearDown(): void
+        {
+            Mockery::close();
+
+            unlink($this->langDir . DS . 'custom.php');
+
+            sleep(1);
+            rmdir($this->langDir);
+
+            sleep(1);
+            rmdir(\Quantum\Libraries\Lang\modules_dir() . DS . \Quantum\Libraries\Lang\current_module() . DS . 'Resources' . DS . 'lang');
+
+            sleep(1);
+            rmdir(\Quantum\Libraries\Lang\modules_dir() . DS . \Quantum\Libraries\Lang\current_module() . DS . 'Resources');
+
+            sleep(1);
+            rmdir(\Quantum\Libraries\Lang\modules_dir() . DS . \Quantum\Libraries\Lang\current_module());
+
+            sleep(1);
+            rmdir(\Quantum\Libraries\Lang\modules_dir());
+        }
+
+        public function testLangLoad()
+        {
+            $this->assertEmpty($this->lang->getTranslations());
+
+            $this->lang->load($this->loaderMock);
+
+            $this->assertNotEmpty($this->lang->getTranslations());
+        }
+
+        public function testLangGetSet()
+        {
+            $this->lang->load($this->loaderMock);
+
+            $this->assertEquals('en', $this->lang->getLang());
+
+            $this->lang->setLang('ru');
+
+            $this->assertEquals('ru', $this->lang->getLang());
+        }
+
+        public function testGetTranslation()
+        {
+            $this->lang->load($this->loaderMock);
+
+            $this->assertEquals('Testing', $this->lang->getTranslation('custom.test'));
+
+            $this->assertEquals('Information about new feature', $this->lang->getTranslation('custom.info', 'new'));
+
+            $this->assertEquals('custom.not-exists', $this->lang->getTranslation('custom.not-exists'));
+        }
+
     }
+
 }
