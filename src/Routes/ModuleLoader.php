27760fc5e9ee005eff073b0c6233d3bcc2ca741c
Arman <armanist@mail.ru>
2020-07-12 20:35:46 +0400

    ModuleLoader enhancements

--- src/Routes/ModuleLoader.php
+++ src/Routes/ModuleLoader.php
@@ -9,14 +9,16 @@
  * @author Arman Ag. <arman.ag@softberg.org>
  * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
  * @link http://quantum.softberg.org/
- * @since 1.0.0
+ * @since 2.0.0
  */
 
 namespace Quantum\Routes;
 
 use Quantum\Exceptions\ModuleLoaderException;
 use Quantum\Exceptions\ExceptionMessages;
+use Quantum\Libraries\Storage\FileSystem;
 use Quantum\Routes\Route;
+use Closure;
 
 /**
  * ModuleLoader Class
@@ -24,7 +26,6 @@ use Quantum\Routes\Route;
  * ModuleLoader class allows loads modules
  * 
  * @package Quantum
- * @subpackage Routes
  * @category Routes
  */
 class ModuleLoader
@@ -32,35 +33,55 @@ class ModuleLoader
 
     /**
      * List of loaded modules
-     * 
      * @var array 
      */
     public $modules = [];
+    
+    /**
+     * Router instance
+     * @var Quantum\Routes\Router 
+     */
+    private $router;
+    
+    /**
+     * FileSystem instance
+     * @var Quantum\Libraries\Storage\FileSystem
+     */
+    private $fileSystem;
+
+    /**
+     * Class constructor.
+     * @param \Quantum\Routes\Router $router
+     */
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+        $this->fileSystem = new FileSystem();
+    }
 
     /**
      * Load Modules
-     * 
-     * @param \Quantum\Routes\Router $router
-     * @throws \Exception When module file is not found
+     * @param Quantum\Routes\Router $router
+     * @throws ModuleLoaderException
      */
-    public function loadModules(Router $router)
+    public function loadModulesRoutes()
     {
-        $this->modules = require_once BASE_DIR . DS . 'config' . DS . 'modules.php';
+        $this->modules = require_once base_dir() . DS . 'config' . DS . 'modules.php';
 
         foreach ($this->modules['modules'] as $module) {
-            if (!file_exists(MODULES_DIR . DS . $module . DS . 'Config' . DS . 'routes.php')) {
+            if (!$this->fileSystem->exists(modules_dir() . DS . $module . DS . 'Config' . DS . 'routes.php')) {
                 throw new ModuleLoaderException(_message(ExceptionMessages::MODULE_NOT_FOUND, $module));
             }
 
-            $routesClosure = require_once MODULES_DIR . DS . $module . DS . 'Config' . DS . 'routes.php';
+            $routesClosure = require_once modules_dir() . DS . $module . DS . 'Config' . DS . 'routes.php';
 
-            if (!$routesClosure instanceof \Closure) {
+            if (!$routesClosure instanceof Closure) {
                 throw new ModuleLoaderException(ExceptionMessages::ROUTES_NOT_CLOSURE);
             }
 
             $route = new Route($module);
             $routesClosure($route);
-            $router->setRoutes(array_merge($router->getRoutes(), $route->getRuntimeRoutes()));
+            $this->router->setRoutes(array_merge($this->router->getRoutes(), $route->getRuntimeRoutes()));
         }
     }
 
