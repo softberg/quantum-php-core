9db0f64620b6f5163533850d6ea5b2d41df64ba1
Arman <armanist84@mail.ru>
2021-06-28 21:42:00 +0400

    Logger class

---
+++ src/Logger/Logger.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.4.0
+ */
+
+namespace Quantum\Logger;
+
+
+use Quantum\Contracts\ReportableInterface;
+use Quantum\Libraries\Storage\FileSystem;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Quantum\Di\Di;
+
+/**
+ * Class Logger
+ * @package Quantum\Logger
+ */
+class Logger implements LoggerInterface
+{
+
+    /**
+     * @var \Quantum\Contracts\ReportableInterface
+     */
+    private $reporter;
+
+    /**
+     * Logger constructor.
+     * @param \Quantum\Contracts\ReportableInterface $reporter
+     */
+    public function __construct(ReportableInterface $reporter)
+    {
+        $this->reporter = $reporter;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function emergency($message, array $context = [])
+    {
+        $this->log(LogLevel::EMERGENCY, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function alert($message, array $context = [])
+    {
+        $this->log(LogLevel::ALERT, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function critical($message, array $context = [])
+    {
+        $this->log(LogLevel::CRITICAL, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function error($message, array $context = [])
+    {
+        $this->log(LogLevel::ERROR, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function warning($message, array $context = [])
+    {
+        $this->log(LogLevel::WARNING, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function notice($message, array $context = [])
+    {
+        $this->log(LogLevel::NOTICE, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function info($message, array $context = [])
+    {
+        $this->log(LogLevel::INFO, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function debug($message, array $context = [])
+    {
+        $this->log(LogLevel::DEBUG, $message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function log($level, $message, array $context = [])
+    {
+        $this->reporter->report($level, $message, $context);
+    }
+
+}
