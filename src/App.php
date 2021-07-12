c53d655a70e575bd6538ce2e367ae37cf3ba2418
Arman <armanist84@mail.ru>
2021-07-12 21:55:12 +0400

    Correcting doc blocks and typos

--- src/App.php
+++ src/App.php
@@ -1,18 +1,46 @@
 <?php
 
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.5.0
+ */
 
 namespace Quantum;
 
-use Quantum\Di\Di;
 use Quantum\Tracer\ErrorHandler;
-use Quantum\Bootstrap;
+use Quantum\Di\Di;
 
+/**
+ * Class App
+ * @package Quantum
+ */
 class App
 {
 
+    /**
+     * Starts the app
+     * @throws \Quantum\Exceptions\CsrfException
+     * @throws \Quantum\Exceptions\RouteException
+     * @throws \Quantum\Exceptions\DiException
+     * @throws \Quantum\Exceptions\MiddlewareException
+     * @throws \Quantum\Exceptions\ControllerException
+     * @throws \Quantum\Exceptions\ModuleLoaderException
+     * @throws \Quantum\Exceptions\LoaderException
+     * @throws \Quantum\Exceptions\LangException
+     * @throws \Quantum\Exceptions\EnvException
+     * @throws \ReflectionException
+     * @throws \ErrorException
+     */
     public static function start()
     {
-        self::loadCoreFuncations();
+        self::loadCoreFunctions();
 
         Di::loadDefinitions();
 
@@ -21,7 +49,10 @@ class App
         Bootstrap::run();
     }
 
-    public static function loadCoreFuncations()
+    /**
+     * Loads the core functions
+     */
+    public static function loadCoreFunctions()
     {
         foreach (glob(HELPERS_DIR . DS . 'functions' . DS . '*.php') as $filename) {
             require_once $filename;
