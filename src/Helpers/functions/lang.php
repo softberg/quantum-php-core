5ec87a72f7f64f685616fd2ec602d9dfe445c6a7
Arman <armanist84@mail.ru>
2021-07-26 21:02:02 +0400

    Correcting the return type and removing function_exists checks

--- src/Helpers/functions/lang.php
+++ src/Helpers/functions/lang.php
@@ -9,51 +9,43 @@
  * @author Arman Ag. <arman.ag@softberg.org>
  * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
  * @link http://quantum.softberg.org/
- * @since 2.0.0
+ * @since 2.5.0
  */
 
 use Quantum\Libraries\Lang\Lang;
 
-if (!function_exists('current_lang')) {
 
-    /**
-     * Gets the current lang
-     * @return string|null
-     */
-    function current_lang(): ?string
-    {
-        return Lang::getInstance()->getLang();
-    }
-
+/**
+ * Gets the current lang
+ * @return string|null
+ */
+function current_lang(): ?string
+{
+    return Lang::getInstance()->getLang();
 }
 
-if (!function_exists('t')) {
 
-    /**
-     * Gets translation
-     * @param string $key
-     * @param mixed $params
-     * @return string
-     */
-    function t(string $key, $params = null): string
-    {
-        return Lang::getInstance()->getTranslation($key, $params);
-    }
-
+/**
+ * Gets translation
+ * @param string $key
+ * @param mixed $params
+ * @return string|null
+ */
+function t(string $key, $params = null): ?string
+{
+    return Lang::getInstance()->getTranslation($key, $params);
 }
 
-if (!function_exists('_t')) {
 
-    /**
-     * Outputs the translation
-     * @param string $key
-     * @param mixed $params
-     */
-    function _t(string $key, $params = null)
-    {
-        echo t($key, $params);
-    }
-
+/**
+ * Outputs the translation
+ * @param string $key
+ * @param mixed $params
+ */
+function _t(string $key, $params = null)
+{
+    echo t($key, $params);
 }
+
 
 
