a1c60f64a56cc8fdcf76c4302d037ee21ab9fe52
Arman <armanist84@mail.ru>
2021-08-24 20:57:51 +0400

    Adding @method annotations

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -19,6 +19,29 @@ use Quantum\Http\Response\HttpResponse;
 /**
  * Class Response
  * @package Quantum\Http
+ * @method static void init()
+ * @method static void flush()
+ * @method static void send()
+ * @method static string getContent()
+ * @method static void setStatusCode(int $code)
+ * @method static int getStatusCode()
+ * @method static string getStatusText()
+ * @method static void redirect(string $url, int $code = null)
+ * @method static void json(array $data = null, int $code = null)
+ * @method static void xml(array $data = null, $root = '<data></data>', int $code = null)
+ * @method static void html(string $html, int $code = null)
+ * @method static bool has(string $key)
+ * @method static mixed get(string $key, string $default = null)
+ * @method static void set(string $key, $value)
+ * @method static array all()
+ * @method static void delete(string $key)
+ * @method static bool hasHeader(string $key))
+ * @method static string getHeader(string $key)
+ * @method static void setHeader(string $key, string $value)
+ * @method static array allHeaders()
+ * @method static void deleteHeader(string $key)
+ * @method static void setContentType(string $contentType)
+ * @method static string|null getContentType()
  * @mixin HttpResponse
  */
 class Response
