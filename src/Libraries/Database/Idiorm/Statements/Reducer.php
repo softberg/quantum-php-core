f8f360e6466926ca6db69bc0afc044f1f031f00b
Arman <armanist84@mail.ru>
2021-12-15 21:59:52 +0400

    More changes on Idiorm DBAL (segregating the interface and adding new reducer trait)

---
+++ src/Libraries/Database/Idiorm/Statements/Reducer.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.6.0
+ */
+
+namespace Quantum\Libraries\Database\Idiorm\Statements;
+
+use Quantum\Libraries\Database\DbalInterface;
+use RecursiveIteratorIterator;
+use RecursiveArrayIterator;
+
+/**
+ * Trait Modifier
+ * @package Quantum\Libraries\Database\Idiorm\Statements
+ */
+trait Reducer
+{
+
+    /**
+     * @inheritDoc
+     * @throws \Quantum\Exceptions\DatabaseException
+     */
+    public function select(...$columns): DbalInterface
+    {
+
+        array_walk($columns, function (&$column) use ($columns) {
+            if (is_array($column)) {
+                $column = array_flip($column);
+            }
+        });
+
+        $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($columns));
+        $columns = iterator_to_array($iterator, true);
+
+        $this->getOrmModel()->select_many($columns);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws \Quantum\Exceptions\DatabaseException
+     */
+    public function groupBy(string $column): DbalInterface
+    {
+        $this->getOrmModel()->group_by($column);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws \Quantum\Exceptions\DatabaseException
+     */
+    public function orderBy(string $column, string $direction): DbalInterface
+    {
+        switch (strtolower($direction)) {
+            case 'asc':
+                $this->getOrmModel()->order_by_asc($column);
+                break;
+            case 'desc':
+                $this->getOrmModel()->order_by_desc($column);
+                break;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws \Quantum\Exceptions\DatabaseException
+     */
+    public function offset(int $offset): DbalInterface
+    {
+        $this->getOrmModel()->offset($offset);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws \Quantum\Exceptions\DatabaseException
+     */
+    public function limit(int $limit): DbalInterface
+    {
+        $this->getOrmModel()->limit($limit);
+        return $this;
+    }
+
+}
