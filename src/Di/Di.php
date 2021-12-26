89ecab52780858dc97c93465b41964b536b01b70
Arman <armanist84@mail.ru>
2021-12-27 00:06:34 +0400

    Autowire user dependencies (adding missing method)

--- src/Di/Di.php
+++ src/Di/Di.php
@@ -14,6 +14,7 @@
 
 namespace Quantum\Di;
 
+use Quantum\Libraries\Storage\FileSystem;
 use Quantum\Exceptions\DiException;
 use ReflectionFunction;
 use ReflectionClass;
@@ -156,6 +157,23 @@ class Di
     protected static function instantiable($type): bool
     {
         return $type != 'Closure' && !is_callable($type) && class_exists($type);
+    }
+
+    /**
+     * Loads user defined dependencies
+     * @return array
+     */
+    private static function userDependencies(): array
+    {
+        $fs = new FileSystem();
+
+        $dependencies = base_dir() . DS . 'config' . DS . 'dependencies.php';
+
+        if (!$fs->exists($dependencies)) {
+            return [];
+        }
+
+        return require_once $dependencies;
     }
 
     /**
