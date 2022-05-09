7c006403868bfd2174cd3ba936833acc8546e1ee
Arman <armanist84@mail.ru>
2022-05-09 21:05:43 +0400

    Separating key definitions

---
+++ src/Libraries/Database/Key.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Quantum\Libraries\Database;
+
+class Key
+{
+
+    const PRIMARY = 'primary';
+    const INDEX = 'index';
+    const UNIQUE = 'unique';
+    const FULLTEXT = 'fulltext';
+    const SPATIAL = 'spatial';
+
+}
