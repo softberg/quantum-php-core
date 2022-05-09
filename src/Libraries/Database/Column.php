3ea7b10266de2ca58404c67b9971b3927b4bbb25
Arman <armanist84@mail.ru>
2022-05-09 21:07:44 +0400

    Adding doc-blocks for Column class and unit testing

--- src/Libraries/Database/Column.php
+++ src/Libraries/Database/Column.php
@@ -1,41 +1,172 @@
 <?php
 
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.7.0
+ */
+
 namespace Quantum\Libraries\Database;
 
+/**
+ * Class Column
+ * @package Quantum\Libraries\Database
+ */
 class Column
 {
 
+    /**
+     * Action add
+     */
     const ADD = 'ADD';
-    const MODIFY = 'MODIFY';
-    const RENAME = 'RENAME';
-    const DROP = 'DROP';
-    const NAME = 'name';
-    const TYPE = 'type';
-    const CONSTRAINT = 'constraint';
-    const ATTRIBUTE = 'attribute';
-    const NULLABLE = 'nullable';
-    const DEFAULT = 'default';
-    const AFTER = 'afterColumn';
-    const AUTO_INCREMENT = 'autoincrement';
-    const KEY_PRIMARY = 'primary';
-    const KEY_INDEX = 'index';
-    const KEY_UNIQUE = 'unique';
-    const KEY_FULLTEXT = 'fulltext';
-    const KEY_SPATIAL = 'spatial';
 
+    /**
+     * Action modify
+     */
+    const MODIFY = 'MODIFY';
+
+    /**
+     * Action rename
+     */
+    const RENAME = 'RENAME';
+
+    /**
+     * Action drop
+     */
+    const DROP = 'DROP';
+
+    /**
+     * Name property
+     */
+    const NAME = 'name';
+
+    /**
+     * Type property
+     */
+    const TYPE = 'type';
+
+    /**
+     * Constraint property
+     */
+    const CONSTRAINT = 'constraint';
+
+    /**
+     * Attribute property
+     */
+    const ATTRIBUTE = 'attribute';
+
+    /**
+     * Nullable property
+     */
+    const NULLABLE = 'nullable';
+
+    /**
+     * Default property
+     */
+    const DEFAULT = 'default';
+
+    /**
+     * After column property
+     */
+    const AFTER = 'afterColumn';
+
+    /**
+     * Comment property
+     */
+    const COMMENT = 'comment';
+
+    /**
+     * Auto increment property
+     */
+    const AUTO_INCREMENT = 'autoincrement';
+
+    /**
+     * Attribute binary
+     */
+    const ATTR_BINARY = 'BINARY';
+
+    /**
+     * Attribute unsigned
+     */
+    const ATTR_UNSIGNED = 'UNSIGNED';
+
+    /**
+     * Attribute zero fill
+     */
+    const ATTR_ZEROFILL = 'UNSIGNED ZEROFILL';
+
+    /**
+     * @var string
+     */
     private $name;
+
+    /**
+     * @var string
+     */
     private $newName;
+
+    /**
+     * @var string
+     */
     private $type;
+
+    /**
+     * @var mixed
+     */
     private $constraint;
+
+    /**
+     * @var string
+     */
     private $attribute = null;
+
+    /**
+     * @var string
+     */
     private $nullable = 'NOT NULL';
+
+    /**
+     * @var string
+     */
     private $default;
+
+    /**
+     * @var bool
+     */
     private $defaultQuoted = true;
+
+    /**
+     * @var string
+     */
     private $autoincrement = null;
+
+    /**
+     * @var string
+     */
     private $indexKey = null;
+
+    /**
+     * @var string
+     */
     private $comment;
+
+    /**
+     * @var string
+     */
     private $afterColumn;
 
+    /**
+     * Column constructor.
+     * @param string $name
+     * @param string $type
+     * @param mixed $constraint
+     */
     public function __construct(string $name, string $type = null, $constraint = null)
     {
         $this->name = $name;
@@ -46,85 +177,140 @@ class Column
         }
     }
 
-    public function renameTo(string $newName)
+    /**
+     * Renames the column
+     * @param string $newName
+     * @return $this
+     */
+    public function renameTo(string $newName): Column
     {
         $this->newName = $newName;
         return $this;
     }
 
+    /**
+     * Gets the column property
+     * @param string $property
+     * @param string $action
+     * @return mixed
+     */
     public function get(string $property, string $action = null)
     {
         return isset($this->$property) && ($action != self::RENAME && $action != self::DROP) ? $this->$property : null;
     }
 
+    /**
+     * Makes the column auto incremental
+     */
     public function autoIncrement()
     {
         $this->autoincrement = 'AUTO_INCREMENT';
         $this->primary();
     }
 
+    /**
+     * Adds a primary key the column
+     */
     public function primary()
     {
-        $this->indexKey = self::KEY_PRIMARY;
+        $this->indexKey = Key::PRIMARY;
     }
 
+    /**
+     * Adds a index key to the column
+     */
     public function index()
     {
-        $this->indexKey = self::KEY_INDEX;
+        $this->indexKey = Key::INDEX;
     }
 
+    /**
+     * Adds unique key to the column
+     */
     public function unique()
     {
-        $this->indexKey = self::KEY_UNIQUE;
+        $this->indexKey = Key::UNIQUE;
     }
 
+    /**
+     * Adds a fulltext key the column
+     */
     public function fulltext()
     {
-        $this->indexKey = self::KEY_FULLTEXT;
+        $this->indexKey = Key::FULLTEXT;
     }
 
+    /**
+     * Adds a spatial key the column
+     */
     public function spatial()
     {
-        $this->indexKey = self::KEY_SPATIAL;
+        $this->indexKey = Key::SPATIAL;
     }
 
+    /**
+     * Adds a type to the column
+     * @param string $type
+     * @param type $constraint
+     */
     public function type(string $type, $constraint = null)
     {
         $this->type = strtoupper($type);
         $this->constraint = $constraint;
     }
 
+    /**
+     * Adds or removes nullable property
+     * @param bool $indeed
+     */
     public function nullable(bool $indeed = true)
     {
         $this->nullable = (!$indeed ? 'NOT ' : '') . 'NULL';
     }
 
+    /**
+     * Adds default value to the column
+     * @param type $value
+     * @param bool $quoted
+     */
     public function default($value, bool $quoted = true)
     {
         $this->default = $value;
         $this->defaultQuoted = $quoted;
     }
 
-    public function attribute(string $value)
+    /**
+     * Adds or removes attribute to the column
+     * @param string $value
+     */
+    public function attribute(?string $value)
     {
-        $availabelAttributes = ['BINARY', 'UNSIGNED', 'UNSIGNED ZEROFILL'];
-
-        if (in_array(strtoupper($value), $availabelAttributes)) {
-            $this->attribute = strtoupper($value);
-        }
+        $this->attribute = $value ? strtoupper($value) : null;
     }
 
-    public function comment(string $comment)
+    /**
+     * Adds or removes comment to the column
+     * @param string $comment
+     */
+    public function comment(?string $comment)
     {
         $this->comment = $comment;
     }
 
+    /**
+     * Adds column after a given column
+     * @param string $columnName
+     */
     public function after(string $columnName)
     {
         $this->afterColumn = $columnName;
     }
 
-    public function defaultQuoted()
+    /**
+     * Adds quotes on default value
+     * @return bool
+     */
+    public function defaultQuoted(): bool
     {
         return $this->defaultQuoted;
     }
---
+++ tests/unit/Libraries/Database/ColumnTest.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Quantum\Tests\Libraries\Database;
+
+use PHPUnit\Framework\TestCase;
+use Quantum\Libraries\Database\Column;
+use Quantum\Libraries\Database\Type;
+use Quantum\Libraries\Database\Key;
+
+class ColumnTest extends TestCase
+{
+
+    public function testColumnConstructor()
+    {
+        $this->assertInstanceOf(Column::class, new Column('test'));
+    }
+
+    public function testColumnRename()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->renameTo('new_test');
+
+        $this->assertEquals('new_test', $column->get('newName'));
+    }
+
+    public function testColumnGet()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $this->assertEquals('test', $column->get(Column::NAME));
+
+        $this->assertEquals('INT', $column->get(Column::TYPE));
+
+        $this->assertEquals(11, $column->get(Column::CONSTRAINT));
+    }
+
+    public function testColumnAutoIncrement()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->autoIncrement();
+
+        $this->assertEquals('AUTO_INCREMENT', $column->get(Column::AUTO_INCREMENT));
+
+        $this->assertEquals('primary', $column->get('indexKey'));
+    }
+
+    public function testColumnPrimaryKey()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->primary();
+
+        $this->assertEquals('primary', $column->get('indexKey'));
+    }
+
+    public function testColumnIndexKey()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->index();
+
+        $this->assertEquals('index', $column->get('indexKey'));
+    }
+
+    public function testColumnUniqueKey()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->unique();
+
+        $this->assertEquals('unique', $column->get('indexKey'));
+    }
+
+    public function testColumnFulltextKey()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->fulltext();
+
+        $this->assertEquals('fulltext', $column->get('indexKey'));
+    }
+
+    public function testColumnSpatialKey()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->spatial();
+
+        $this->assertEquals('spatial', $column->get('indexKey'));
+    }
+
+    public function testAddTypeToColumn()
+    {
+        $column = new Column('test');
+
+        $column->type(Type::VARCHAR, 255);
+
+        $this->assertEquals('VARCHAR', $column->get(Column::TYPE));
+
+        $this->assertEquals(255, $column->get(Column::CONSTRAINT));
+
+        $column->type(Type::BOOL);
+
+        $this->assertEquals('BOOL', $column->get(Column::TYPE));
+    }
+
+    public function testAddOrRemoveColumnNullableProperty()
+    {
+        $column = new Column('test', Type::VARCHAR, 255);
+
+        $this->assertEquals('NOT NULL', $column->get(Column::NULLABLE));
+
+        $column->nullable();
+
+        $this->assertEquals('NULL', $column->get(Column::NULLABLE));
+
+        $column->nullable(false);
+
+        $this->assertEquals('NOT NULL', $column->get(Column::NULLABLE));
+    }
+
+    public function testAddOrRemoveColumnDefaultValue()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $this->assertNull($column->get(Column::DEFAULT));
+
+        $column->default(5);
+
+        $this->assertEquals(5, $column->get(Column::DEFAULT));
+
+        $column->default(null);
+
+        $this->assertNull($column->get(Column::DEFAULT));
+    }
+
+    public function testAddOrRemoveColumnAttribute()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $this->assertNull($column->get(Column::ATTRIBUTE));
+
+        $column->attribute(Column::ATTR_BINARY);
+
+        $this->assertEquals('BINARY', $column->get(Column::ATTRIBUTE));
+
+        $column->attribute(Column::ATTR_UNSIGNED);
+
+        $this->assertEquals('UNSIGNED', $column->get(Column::ATTRIBUTE));
+
+        $column->attribute(null);
+
+        $this->assertNull($column->get(Column::ATTRIBUTE));
+    }
+
+    public function testAddOrRemoveColumnComment()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $this->assertNull($column->get(Column::COMMENT));
+
+        $column->comment('This is test column');
+
+        $this->assertEquals('This is test column', $column->get(Column::COMMENT));
+
+        $column->comment(null);
+
+        $this->assertNull($column->get(Column::COMMENT));
+    }
+
+    public function testAfterColumn()
+    {
+        $column = new Column('test', Type::INT, 11);
+
+        $column->after('prev');
+
+        $this->assertEquals('prev', $column->get(Column::AFTER));
+    }
+
+}
