13b9903de34ce1f0e5ff590856fcd7b5f87bde16
Arman <armanist84@mail.ru>
2022-05-12 21:05:13 +0400

    Correcting the Table class and adding doc-blocks

--- src/Libraries/Database/Table.php
+++ src/Libraries/Database/Table.php
@@ -1,19 +1,34 @@
 <?php
 
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.7.0
+ */
+
 namespace Quantum\Libraries\Database;
 
 use Quantum\Exceptions\MigrationException;
 
 /**
- *
+ * Class Table
+ * @package Quantum\Libraries\Database
+ * 
  * @method self autoIncrement()
  * @method self primary()
  * @method self index()
  * @method self unique()
  * @method self fulltext()
  * @method self spatial()
- * @method self nullable()
- * @method self default($value)
+ * @method self nullable(bool $indeed)
+ * @method self default($value, bool $quoted)
+ * @method self defaultQuoted(type $paramName) Description
  * @method self attribute(string $value)
  * @method self comment(string $value)
  * @method self type(string $type, $constraint)
@@ -21,15 +36,35 @@ use Quantum\Exceptions\MigrationException;
 class Table
 {
 
+    /**
+     * Action create
+     */
     const CREATE = 1;
+
+    /**
+     * Action alter
+     */
     const ALTER = 2;
+
+    /**
+     * Action drop
+     */
     const DROP = 3;
+
+    /**
+     * Action rename
+     */
     const RENAME = 4;
 
     /**
      * @var string
      */
     private $name;
+
+    /**
+     * 
+     * @var string
+     */
     private $newName;
 
     /**
@@ -41,13 +76,25 @@ class Table
      * @var array
      */
     private $columns = [];
+
+    /**
+     * @var array
+     */
     private $indexKeys = [];
 
+    /**
+     * Table constructor.
+     * @param string $name
+     */
     public function __construct(string $name)
     {
         $this->name = $name;
     }
 
+    /**
+     * Table destructor
+     * Saves the data before object goes out of scope
+     */
     public function __destruct()
     {
         $this->save();
@@ -59,7 +106,13 @@ class Table
         return $this;
     }
 
-    public function setAction(int $action, ?array $data = null)
+    /**
+     * Sets an action on a table to be performed 
+     * @param int $action
+     * @param array|null $data
+     * @return Table
+     */
+    public function setAction(int $action, ?array $data = null): Table
     {
         $this->action = $action;
 
@@ -72,11 +125,13 @@ class Table
     }
 
     /**
+     * Adds column to the table
      * @param string $name
      * @param string $type
-     * @param $constraint
+     * @param mixed $constraint
+     * @return Table
      */
-    public function addColumn(string $name, string $type, $constraint = null)
+    public function addColumn(string $name, string $type, $constraint = null): Table
     {
         array_push($this->columns, [
             'column' => new Column($name, $type, $constraint),
@@ -86,32 +141,58 @@ class Table
         return $this;
     }
 
-    public function modifyColumn(string $name, string $type = null, $constraint = null)
+    /**
+     * Modifies the column
+     * @param string $name
+     * @param string $type
+     * @param mixed $constraint
+     * @return Table
+     */
+    public function modifyColumn(string $name, string $type = null, $constraint = null): Table
     {
-        array_push($this->columns, [
-            'column' => new Column($name, $type, $constraint),
-            'action' => Column::MODIFY
-        ]);
+        if ($this->action == self::ALTER) {
+            array_push($this->columns, [
+                'column' => new Column($name, $type, $constraint),
+                'action' => Column::MODIFY
+            ]);
+        }
 
         return $this;
     }
 
-    public function renameColumn(string $oldName, string $newName)
+    /**
+     * Renames the column name
+     * @param string $oldName
+     * @param string $newName
+     * @return Table
+     */
+    public function renameColumn(string $oldName, string $newName): Table
     {
-        array_push($this->columns, [
-            'column' => (new Column($oldName))->renameTo($newName),
-            'action' => Column::RENAME
-        ]);
+        if ($this->action == self::ALTER) {
+            array_push($this->columns, [
+                'column' => (new Column($oldName))->renameTo($newName),
+                'action' => Column::RENAME
+            ]);
+        }
 
         return $this;
     }
 
-    public function dropColumn(string $name)
+    /**
+     * Drops the column
+     * @param string $name
+     * @return Table
+     */
+    public function dropColumn(string $name): Table
     {
-        array_push($this->columns, [
-            'column' => new Column($name),
-            'action' => Column::DROP
-        ]);
+        if ($this->action == self::ALTER) {
+            array_push($this->columns, [
+                'column' => new Column($name),
+                'action' => Column::DROP
+            ]);
+        }
+        
+        return $this;
     }
 
     public function after(string $columnName)
@@ -121,23 +202,14 @@ class Table
         return $this;
     }
 
-    public function __call(string $method, $arguments)
+    /**
+     * Gets the generated query
+     * @return string
+     */
+    public function getSql(): string
     {
-        if (!method_exists(Column::class, $method)) {
-            throw MigrationException::methodNotDefined($method);
-        }
+        $sql = '';
 
-        $this->columns[$this->columnKey()]['column']->{$method}(...$arguments);
-        return $this;
-    }
-
-    protected function save()
-    {
-        Database::execute($this->getSql());
-    }
-
-    protected function getSql()
-    {
         switch ($this->action) {
             case self::CREATE:
                 $sql = $this->createTableSql();
@@ -156,58 +228,133 @@ class Table
         return $sql;
     }
 
+    /**
+     * Allows to call methods of Column class 
+     * @param string $method
+     * @param array|null $arguments
+     * @return $this
+     * @throws Quantum\Exceptions\MigrationException
+     */
+    public function __call(string $method, ?array $arguments)
+    {
+        if (!method_exists(Column::class, $method)) {
+            throw MigrationException::methodNotDefined($method);
+        }
+
+        $this->columns[$this->columnKey()]['column']->{$method}(...$arguments);
+        return $this;
+    }
+
+    /**
+     * Saves the query
+     */
+    protected function save()
+    {
+        $sql = $this->getSql();
+
+        if ($sql) {
+            Database::execute($sql);
+        }
+    }
+
+    /**
+     * Generates create table statement
+     * @return string
+     */
     protected function createTableSql()
     {
-        $sql = 'CREATE TABLE `' . $this->name . '` (';
-        $sql .= $this->columnsSql();
-        $sql .= $this->indexesSql();
-        $sql .= ');';
+        $columnsSql = $this->columnsSql();
+        $indexesSql = $this->indexesSql();
+        $sql = '';
+
+        if (!empty($columnsSql)) {
+            $sql = 'CREATE TABLE `' . $this->name . '` (';
+            $sql .= $columnsSql;
+            $sql .= $indexesSql;
+            $sql .= ');';
+        }
 
         return $sql;
     }
 
+    /**
+     * Generates alter table statement
+     * @return string
+     */
     protected function alterTableSql(): string
     {
-        $sql = 'ALTER TABLE `' . $this->name . '` ';
-        $sql .= $this->columnsSql();
-        $sql .= $this->indexesSql();
-        $sql .= ';';
+        $columnsSql = $this->columnsSql();
+        $indexesSql = $this->indexesSql();
+        $sql = '';
+
+        if (!empty($columnsSql) || !empty($indexesSql)) {
+            $sql = 'ALTER TABLE `' . $this->name . '` ';
+            $sql .= $columnsSql;
+            $sql .= $indexesSql;
+            $sql .= ';';
+        }
 
         return $sql;
     }
 
+    /**
+     * Prepares rename table statement
+     * @return string
+     */
     protected function renameTableSql(): string
     {
         return 'RENAME TABLE `' . $this->name . '` TO `' . $this->newName . '`;';
     }
 
+    /**
+     * Prepares drop table statement
+     * @return string
+     */
     protected function dropTableSql(): string
     {
         return 'DROP TABLE `' . $this->name . '`';
     }
 
+    /**
+     * Prepares columns statements for table
+     * @return string
+     */
     protected function columnsSql(): string
     {
-        $columns = [];
+        $sql = '';
 
-        foreach ($this->columns as $entry) {
-            $columnString = ($entry['action'] ? $entry['action'] . ' COLUMN ' : '');
-            $columnString .= $this->composeColumn($entry['column'], $entry['action']);
+        if ($this->columns) {
 
-            if ($entry['column']->get('indexKey')) {
-                $this->indexKeys[$entry['column']->get('indexKey')][] = $entry['column']->get('name');
+            $columns = [];
+
+            foreach ($this->columns as $entry) {
+                $columnString = ($entry['action'] ? $entry['action'] . ' COLUMN ' : '');
+                $columnString .= $this->composeColumn($entry['column'], $entry['action']);
+
+                if ($entry['column']->get('indexKey')) {
+                    $this->indexKeys[$entry['column']->get('indexKey')][] = $entry['column']->get('name');
+                }
+
+                array_push($columns, $columnString);
             }
 
-            array_push($columns, $columnString);
+            $sql = implode(', ', $columns);
         }
 
-        return implode(', ', $columns);
+        return $sql;
     }
 
+    /**
+     * Composes the column 
+     * @param Column $column
+     * @param string $action
+     * @return string
+     */
     protected function composeColumn(Column $column, string $action = null): string
     {
         return
                 $this->columnAttrSql($column->get(Column::NAME), '`', '`') .
+                $this->columnAttrSql($column->get(Column::NEW_NAME), ' TO `', '`') .
                 $this->columnAttrSql($column->get(Column::TYPE), ' ') .
                 $this->columnAttrSql($column->get(Column::CONSTRAINT), '(', ')') .
                 $this->columnAttrSql($column->get(Column::ATTRIBUTE), ' ') .
@@ -218,6 +365,13 @@ class Table
                 $this->columnAttrSql($column->get(Column::AUTO_INCREMENT), ' ');
     }
 
+    /**
+     * Prepares column attributes
+     * @param string|null $definition
+     * @param string $before
+     * @param string $after
+     * @return string
+     */
     protected function columnAttrSql(?string $definition, string $before = '', string $after = ''): string
     {
         $sql = '';
@@ -229,6 +383,10 @@ class Table
         return $sql;
     }
 
+    /**
+     * Prepares statement for primary key
+     * @return string
+     */
     protected function primaryKeysSql(): string
     {
         $sql = '';
@@ -240,6 +398,11 @@ class Table
         return $sql;
     }
 
+    /**
+     * Prepares statement for index keys
+     * @param string $type
+     * @return string
+     */
     protected function indexKeysSql(string $type): string
     {
         $sql = '';
@@ -255,6 +418,10 @@ class Table
         return $sql;
     }
 
+    /**
+     * Builds a complete statement for index keys
+     * @return string
+     */
     protected function indexesSql(): string
     {
         return $this->primaryKeysSql() .
@@ -264,6 +431,11 @@ class Table
                 $this->indexKeysSql(Key::SPATIAL);
     }
 
+    /**
+     * Checks if column exists on a table 
+     * @param string $columnName
+     * @return bool
+     */
     private function checkColumnExists(string $columnName): bool
     {
         $columnIndex = null;
@@ -280,6 +452,10 @@ class Table
         return !is_null($columnIndex);
     }
 
+    /**
+     * Gets the column key
+     * @return int
+     */
     private function columnKey(): int
     {
         return (int) array_key_last($this->columns);
