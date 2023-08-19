bd251954634da29cfc20571ba53569020d544f32
Arman <armanist@mail.com>
2023-08-19 17:52:13 +0400

    Asset library enhancements

--- src/Exceptions/AssetException.php
+++ src/Exceptions/AssetException.php
@@ -9,7 +9,7 @@
  * @author Arman Ag. <arman.ag@softberg.org>
  * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
  * @link http://quantum.softberg.org/
- * @since 2.8.0
+ * @since 2.9.0
  */
 
 namespace Quantum\Exceptions;
@@ -23,10 +23,21 @@ class AssetException extends \Exception
     /**
      * @param string $position
      * @param string $name
-     * @return \Quantum\Exceptions\AssetException
+     * @return AssetException
+     * @throws LangException
      */
     public static function positionInUse(string $position, string $name): AssetException
     {
         return new self(t('exception.position_in_use', [$position, $name]), E_WARNING);
+    }
+
+    /**
+     * @param string|null $name
+     * @return AssetException
+     * @throws LangException
+     */
+    public static function nameInUse(?string $name): AssetException
+    {
+        return new self(t('exception.name_in_use', [$name]), E_WARNING);
     }
 }
---
+++ tests/unit/Libraries/Asset/AssetTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Quantum\Tests\Libraries\Asset;
+
+use Quantum\Libraries\Asset\Asset;
+use Quantum\Tests\AppTestCase;
+
+class AssetTest extends AppTestCase
+{
+
+    private $asset;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('base_url', 'http://mydomain.com');
+
+        $this->asset = new Asset(Asset::JS, 'scripts/jquery.js', 'jQuery');
+
+    }
+
+    public function testAssetType()
+    {
+        $this->assertEquals(Asset::JS, $this->asset->getType());
+    }
+
+    public function testAssetPath()
+    {
+        $this->assertEquals('scripts/jquery.js', $this->asset->getPath());
+    }
+
+    public function testAssetName()
+    {
+        $this->assertEquals('jQuery', $this->asset->getName());
+    }
+
+    public function testAssetPosition()
+    {
+        $this->assertEquals(-1, $this->asset->getPosition());
+    }
+
+    public function testAssetAttributes()
+    {
+        $this->assertIsArray($this->asset->getAttributes());
+
+        $this->assertCount(0, $this->asset->getAttributes());
+    }
+
+    public function testAssetUrl()
+    {
+        $this->assertEquals(
+            'http://mydomain.com/assets/scripts/jquery.js',
+            $this->asset->url());
+    }
+
+    public function testAssetTag()
+    {
+        $this->assertEquals(
+            '<script src="http://mydomain.com/assets/scripts/jquery.js" ></script>',
+            $this->asset->tag());
+    }
+
+}
