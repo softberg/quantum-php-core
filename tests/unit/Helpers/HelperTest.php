4c5ac4a61227cbce7065fa099479e105030cdc45
Arman <armanist@mail.com>
2024-08-14 20:05:22 +0400

    Updating tests related to AssetManager refactoring

--- tests/unit/Helpers/HelperTest.php
+++ tests/unit/Helpers/HelperTest.php
@@ -126,11 +126,13 @@ class HelperTest extends AppTestCase
 
         asset()->register([
             new Asset(Asset::JS, 'js/bootstrap.js'),
-            new Asset(Asset::JS, 'js/bootstrap-datepicker.min.js')
+            new Asset(Asset::JS, 'js/bootstrap-datepicker.min.js'),
+            new Asset(Asset::JS, 'js/jQuery.js', 'jQuery', 0)
         ]);
 
         $expectedOutput = '<link rel="stylesheet" type="text/css" href="' . asset()->url('css/style.css') . '">' . PHP_EOL .
             '<link rel="stylesheet" type="text/css" href="' . asset()->url('css/responsive.css') . '">' . PHP_EOL .
+            '<script src="' . asset()->url('js/jQuery.js') . '" ></script>' . PHP_EOL .
             '<script src="' . asset()->url('js/bootstrap.js') . '" ></script>' . PHP_EOL .
             '<script src="' . asset()->url('js/bootstrap-datepicker.min.js') . '" ></script>' . PHP_EOL;
 
@@ -138,7 +140,8 @@ class HelperTest extends AppTestCase
 
         assets('css');
         assets('js');
-
+// dump(ob_get_contents());
+// dd($expectedOutput);
         $this->assertStringContainsString($expectedOutput, ob_get_contents());
 
         ob_get_clean();
