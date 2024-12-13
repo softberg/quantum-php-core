6bc2ab3c0279af00442f0140172aad28aa06b88e
Arman <407448+armanist@users.noreply.github.com>
2024-12-13 15:42:00 +0400

    Fixing the service path

--- tests/unit/_root/shared/config/auth.php
+++ tests/unit/_root/shared/config/auth.php
@@ -2,5 +2,5 @@
 
 return [
     'type' => 'web',
-    'service' => Quantum\Tests\_root\modules\test\Services\AuthService::class
+    'service' => Quantum\Tests\_root\modules\Test\Services\AuthService::class
 ];
