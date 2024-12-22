fc318070a008a9aa3ff86987eb07af3443d6c46e
Arman <407448+armanist@users.noreply.github.com>
2024-12-22 15:39:51 +0400

    Refactoring of Archive library

--- src/Libraries/Archive/ArchiveException.php
+++ src/Libraries/Archive/ArchiveException.php
@@ -9,34 +9,26 @@
  * @author Arman Ag. <arman.ag@softberg.org>
  * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
  * @link http://quantum.softberg.org/
- * @since 2.9.0
+ * @since 2.9.5
  */
 
-namespace Quantum\Exceptions;
+namespace Quantum\Libraries\Archive;
+
+use Quantum\Exceptions\AppException;
 
 /**
  * Class CacheException
- * @package Quantum\Exceptions
+ * @package Quantum\Libraries\Archive
  */
 class ArchiveException extends AppException
 {
     /**
      * @param string $name
      * @return ArchiveException
-     * @throws LangException
      */
     public static function cantOpen(string $name): ArchiveException
     {
         return new static(t('exception.cant_open', $name), E_WARNING);
     }
 
-    /**
-     * @param string $name
-     * @return ArchiveException
-     * @throws LangException
-     */
-    public static function fileNotFound(string $name): ArchiveException
-    {
-        return new static(t('exception.file_not_found', $name), E_WARNING);
-    }
 }
