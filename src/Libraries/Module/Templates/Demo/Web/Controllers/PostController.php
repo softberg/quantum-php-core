30bc3b69e64da3bf01b2de3af6d9f71bfc6663b4
live-soft <livesoftlive@gmail.com>
2025-01-09 18:21:37 +0400

    Search Functionality #109

--- src/Libraries/Module/Templates/Demo/Web/Controllers/PostController.php
+++ src/Libraries/Module/Templates/Demo/Web/Controllers/PostController.php
@@ -80,7 +80,9 @@ class PostController extends BaseController
         $perPage = $request->get(\'per_page\', self::POSTS_PER_PAGE);
         $currentPage = $request->get(\'page\', self::CURRENT_PAGE);
         
-        $paginatedPosts = $this->postService->getPosts($perPage, $currentPage);
+        $search = trim($request->get(\'q\'));
+        
+        $paginatedPosts = $this->postService->getPosts($perPage, $currentPage, $search);
         
         $view->setParams([
             \'title\' => t(\'common.posts\') . \' | \' . config()->get(\'app_name\'),
