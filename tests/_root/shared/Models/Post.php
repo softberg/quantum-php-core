a6292808860bde5f39fdf686480df9d1268f801d
Arman <407448+armanist@users.noreply.github.com>
2025-04-08 16:06:28 +0400

    Correcting Post model

--- tests/_root/shared/Models/Post.php
+++ tests/_root/shared/Models/Post.php
@@ -9,22 +9,13 @@ class Post extends QtModel
 
     public $idColumn = 'id';
 
-    public $table = 'users';
+    public $table = 'posts';
 
     public $fillable = [
-        'uuid',
-        'firstname',
-        'lastname',
-        'role',
-        'email',
-        'password',
-        'activation_token',
-        'remember_token',
-        'reset_token',
-        'access_token',
-        'refresh_token',
-        'otp',
-        'otp_expires',
-        'otp_token',
+        'title',
+        'content',
+        'author',
+        'published_at',
+        'created_at'
     ];
 }
