351939aba98f9146d5cb438d3d99b264d3b6565b
Arman <407448+armanist@users.noreply.github.com>
2025-07-18 19:32:02 +0400

    Adding new ReservedKeys class as App level constants to prevent setting reserved keys

---
+++ src/App/Constants/ReservedKeys.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Quantum PHP Framework
+ *
+ * An open source software development framework for PHP
+ *
+ * @package Quantum
+ * @author Arman Ag. <arman.ag@softberg.org>
+ * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
+ * @link http://quantum.softberg.org/
+ * @since 2.9.8
+ */
+
+namespace Quantum\App\Constants;
+
+/**
+ * Class ReservedKeys
+ * @package Quantum\App
+ */
+final class ReservedKeys
+{
+
+    /**
+     * Internal response key for rendered view
+     */
+    public const RENDERED_VIEW = '_qt_rendered_view';
+
+    /**
+     * Internal session key for previous request
+     */
+    public const PREV_REQUEST = '_qt_prev_request';
+
+}
