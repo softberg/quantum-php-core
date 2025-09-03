5875de0ab261baa3c1a7332b703eb72ac1d1e015
Arman <407448+armanist@users.noreply.github.com>
2025-09-03 19:33:08 +0400

    Refactor Di Container to Support Abstract-to-Concrete Bindings

--- src/Di/Exceptions/DiException.php
+++ src/Di/Exceptions/DiException.php
@@ -9,7 +9,7 @@
  * @author Arman Ag. <arman.ag@softberg.org>
  * @copyright Copyright (c) 2018 Softberg LLC (https://softberg.org)
  * @link http://quantum.softberg.org/
- * @since 2.9.7
+ * @since 2.9.8
  */
 
 namespace Quantum\Di\Exceptions;
@@ -26,8 +26,40 @@ class DiException extends BaseException
      * @param string $name
      * @return DiException
      */
-    public static function dependencyNotRegistered(string $name): DiException
+    public static function dependencyNotRegistered($name): DiException
     {
         return new self(t('exception.dependency_not_registered', $name), E_ERROR);
+    }
+
+    public static function dependencyAlreadyRegistered($name): DiException
+    {
+        return new self(t('exception.dependency_already_registered', $name), E_ERROR);
+    }
+
+    /**
+     * @param $name
+     * @return DiException
+     */
+    public static function dependencyNotInstantiable($name): DiException
+    {
+        return new self(t('exception.dependency_not_instantiable', $name), E_ERROR);
+    }
+
+    /**
+     * @param string $name
+     * @return DiException
+     */
+    public static function invalidAbstractDependency(string $name): DiException
+    {
+        return new self(t('exception.invalid_abstract_dependency', $name), E_ERROR);
+    }
+
+    /**
+     * @param string $chain
+     * @return DiException
+     */
+    public static function circularDependency(string $chain): DiException
+    {
+        return new self("Circular dependency detected: $chain", 0);
     }
 }
