abbb9424713a58057f3d2aa4d0f2eaa6804ad67b
Arman <407448+armanist@users.noreply.github.com>
2026-04-29 18:24:03 +0400

    [#481] Add Asset exception factory coverage tests

---
+++ tests/Unit/Asset/AssetExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Quantum\Tests\Unit\Asset;
+
+use Quantum\Asset\Exceptions\AssetException;
+use Quantum\Tests\Unit\AppTestCase;
+
+class AssetExceptionTest extends AppTestCase
+{
+    public function testPositionInUse(): void
+    {
+        $exception = AssetException::positionInUse(7, 'styles.css');
+
+        $this->assertInstanceOf(AssetException::class, $exception);
+        $this->assertSame('Position `7` for asset `styles.css` is in use', $exception->getMessage());
+        $this->assertSame(E_WARNING, $exception->getCode());
+    }
+
+    public function testNameInUse(): void
+    {
+        $exception = AssetException::nameInUse('main-style');
+
+        $this->assertInstanceOf(AssetException::class, $exception);
+        $this->assertSame('The name main-style is in use', $exception->getMessage());
+        $this->assertSame(E_WARNING, $exception->getCode());
+    }
+}
+
