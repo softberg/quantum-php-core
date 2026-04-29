eb38106aef2f40b0960cdd22035688a1c26c907f
Arman <407448+armanist@users.noreply.github.com>
2026-04-29 18:21:52 +0400

    [#481] Add Archive exception factory coverage tests

---
+++ tests/Unit/Archive/ArchiveExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Quantum\Tests\Unit\Archive;
+
+use Quantum\Archive\Exceptions\ArchiveException;
+use Quantum\Tests\Unit\AppTestCase;
+
+class ArchiveExceptionTest extends AppTestCase
+{
+    public function testCantOpen(): void
+    {
+        $exception = ArchiveException::cantOpen('demo.zip');
+
+        $this->assertInstanceOf(ArchiveException::class, $exception);
+        $this->assertSame('The archive `demo.zip` can not be opened', $exception->getMessage());
+        $this->assertSame(E_WARNING, $exception->getCode());
+    }
+
+    public function testMissingArchiveName(): void
+    {
+        $exception = ArchiveException::missingArchiveName();
+
+        $this->assertInstanceOf(ArchiveException::class, $exception);
+        $this->assertSame('Archive name is not set', $exception->getMessage());
+        $this->assertSame(E_WARNING, $exception->getCode());
+    }
+}
+
