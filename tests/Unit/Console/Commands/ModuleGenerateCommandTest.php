f85096ec54470fe6c430f32f330ee758586cef56
Arman <407448+armanist@users.noreply.github.com>
2026-04-30 11:37:44 +0400

    [#479] Harden module command test cleanup safety

--- tests/Unit/Console/Commands/ModuleGenerateCommandTest.php
+++ tests/Unit/Console/Commands/ModuleGenerateCommandTest.php
@@ -10,15 +10,20 @@ use Quantum\App\App;
 class ModuleGenerateCommandTest extends AppTestCase
 {
     private ModuleGenerateCommand $command;
-    private string $moduleName = 'Issue479Module';
+    private string $moduleName;
     private string $modulesConfigPath;
+    private string $modulePath;
+    private bool $moduleCreatedByTest = false;
+    private bool $moduleConfigCreatedByTest = false;
 
     public function setUp(): void
     {
         parent::setUp();
 
         $this->command = new ModuleGenerateCommand();
+        $this->moduleName = str_replace('.', '_', uniqid('Issue479Module_', true));
         $this->modulesConfigPath = App::getBaseDir() . DS . 'shared' . DS . 'config' . DS . 'modules.php';
+        $this->modulePath = App::getBaseDir() . DS . 'modules' . DS . $this->moduleName;
     }
 
     public function testCommandMetadata(): void
@@ -51,26 +56,42 @@ class ModuleGenerateCommandTest extends AppTestCase
             '--with-assets' => false,
         ]);
 
-        $modulePath = App::getBaseDir() . DS . 'modules' . DS . $this->moduleName;
-        $this->assertTrue($this->fs->isDirectory($modulePath));
+        $this->assertTrue($this->fs->isDirectory($this->modulePath));
+        $this->moduleCreatedByTest = true;
+
+        $moduleConfigs = $this->fs->require($this->modulesConfigPath);
+        if (is_array($moduleConfigs) && isset($moduleConfigs[$this->moduleName])) {
+            $this->moduleConfigCreatedByTest = true;
+        }
+
         $this->assertStringContainsString($this->moduleName . ' module successfully created', $tester->getDisplay());
     }
 
     public function tearDown(): void
     {
-        $moduleConfigs = $this->fs->require($this->modulesConfigPath);
-        if (isset($moduleConfigs[$this->moduleName])) {
-            unset($moduleConfigs[$this->moduleName]);
+        if ($this->fs->exists($this->modulesConfigPath)) {
+            $moduleConfigs = $this->fs->require($this->modulesConfigPath);
+            if (
+                $this->moduleConfigCreatedByTest
+                && is_array($moduleConfigs)
+                && isset($moduleConfigs[$this->moduleName])
+            ) {
+                unset($moduleConfigs[$this->moduleName]);
 
-            $this->fs->put(
-                $this->modulesConfigPath,
-                "<?php\n\nreturn " . export($moduleConfigs) . ";\n"
-            );
+                $this->fs->put(
+                    $this->modulesConfigPath,
+                    "<?php\n\nreturn " . export($moduleConfigs) . ";\n"
+                );
+            }
         }
 
-        $modulePath = App::getBaseDir() . DS . 'modules' . DS . $this->moduleName;
-        if ($this->fs->isDirectory($modulePath)) {
-            deleteDirectoryWithFiles($modulePath);
+        if (
+            $this->moduleCreatedByTest
+            && $this->fs->isDirectory($this->modulePath)
+            && basename($this->modulePath) === $this->moduleName
+            && strpos($this->moduleName, 'Issue479Module_') === 0
+        ) {
+            deleteDirectoryWithFiles($this->modulePath);
         }
 
         parent::tearDown();
